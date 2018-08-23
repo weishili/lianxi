047ad06ea5798fdba516f42e1dae4dfb64b90643
weishili <389964145@qq.com>
2018-08-23 12:00:47 +0800

    收工

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import '../项目中用到的静态页面资源/01-首页/01-首页顶部.html';
+// import ''
 
-Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App)
